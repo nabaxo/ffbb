4ca1c0de42109679a6adef937feb8e32899f528e
Nebez Kassem <nabaxo@gmail.com>
2020-10-27 19:44:33 +0100

    Correct indendation

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,40 @@ import { createBrowserHistory } from 'history';
 
 
 function App() {
-  const history = createBrowserHistory();
+    const history = createBrowserHistory();
 
-  return (
-    <Router history={history}>
-      <div className="App">
-        <header className="App-header">
-          <h1>Test</h1>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to='/list'>List</NavLink>
-              </li>
-              <li>
-                <NavLink to='/submit-new'>Submit new entry</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main className="App-body">
-          <Switch>
-            <Route exact path='/'>
-              <Redirect to='/list' />
-            </Route>
-            <Route path='list'>
+    return (
+        <Router history={history}>
+            <div className="App">
+                <header className="App-header">
+                    <h1>Test</h1>
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to='/list'>List</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/submit-new'>Submit new entry</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <main className="App-body">
+                    <Switch>
+                        <Route exact path='/'>
+                            <Redirect to='/list' />
+                        </Route>
+                        <Route path='list'>
 
-            </Route>
-            <Route path='submit-new'>
+                        </Route>
+                        <Route path='submit-new'>
 
-            </Route>
-          </Switch>
-        </main>
-      </div>
-    </Router>
-  );
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
