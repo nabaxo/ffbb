2712110301d0dec7cd1eb66242a39a0bd3b3660e
Nebez Kassem <nabaxo@gmail.com>
2020-10-27 18:46:52 +0100

    Add react-dom-router and firebase connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Redirect, Route, Router, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 
 function App() {
+  const history = createBrowserHistory();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Test</h1>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to='/list'>List</NavLink>
+              </li>
+              <li>
+                <NavLink to='/submit-new'>Submit new entry</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="App-body">
+          <Switch>
+            <Route exact path='/'>
+              <Redirect to='/list' />
+            </Route>
+            <Route path='list'>
+
+            </Route>
+            <Route path='submit-new'>
+
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
