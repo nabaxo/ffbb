da8626968d64b14532abf092c38157e200b63743
Nebez Kassem <nabaxo@gmail.com>
2020-11-04 19:41:47 +0100

    Move Firebase-config outside index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import firebase from 'firebase/app';
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+import { firebaseConfig } from './firebase-config';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
