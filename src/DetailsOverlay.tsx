416959f9786ef28ef7ba7f176ac3446b3d3325e3
Nebez Kassem <nabaxo@gmail.com>
2021-06-16 15:44:34 +0200

    Overlay paragraph now receives focus on open

--- src/DetailsOverlay.tsx
+++ src/DetailsOverlay.tsx
@@ -1,15 +1,24 @@
+import { useEffect, useRef } from "react";
+
 interface IProps {
     text: string;
     setDetails: (text: string) => void;
 }
 
 export default function DetailsOverlay({ text, setDetails }: IProps) {
+    const ref = useRef<HTMLParagraphElement>(null);
+
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.focus();
+        }
+    }, []);
 
     return (
         <div className="column overlay">
             <div className="overlay-bg" onClick={() => setDetails('')}></div>
             <div className="column overlay-container">
-                <p className="overlay-text preserve-whitespace">{text}</p>
+                <p tabIndex={0} ref={ref} className="overlay-text preserve-whitespace">{text}</p>
                 <div className="close-x" onClick={() => setDetails('')}>🗙</div>
             </div>
         </div>
